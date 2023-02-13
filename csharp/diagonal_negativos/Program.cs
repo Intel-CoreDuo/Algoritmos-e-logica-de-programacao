06ffd572944dc62fb9fc7c6567fce8433bc22997
Intel-CoreDuo <125302044+Intel-CoreDuo@users.noreply.github.com>
2023-02-13 16:44:58 -0300

    Add files via upload

---
+++ csharp/diagonal_negativos/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace diagonal_negativos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, N;
+
+            Console.Write("Qual a ordem da matriz? ");
+            N = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[N,N];
+
+            for(i = 0; i < N; i++)
+            {
+                for (j = 0; j < N; j++)
+                {
+                    Console.Write("Elemento [" + i + "," + j + "]: ");
+                    matriz[i,j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("DIAGONAL PRINCIPAL: ");
+            int negativos = 0;
+
+            for (i = 0; i < N; i++)
+            {
+                for (j = 0; j < N; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write(matriz[i,j] + " ");
+                    }
+                    if (matriz[i,j] < 0)
+                    {
+                        negativos++;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("QUANTIDADE DE NEGATIVOS = " + negativos);
+
+        }
+    }
+}
