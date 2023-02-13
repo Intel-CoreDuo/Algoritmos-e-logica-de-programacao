8ddbdab17081cf4d657cb9ae81b11413d445ed58
Intel-CoreDuo <125302044+Intel-CoreDuo@users.noreply.github.com>
2023-02-13 16:50:57 -0300

    Add files via upload

---
+++ csharp/retangulo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace retangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double x, y, area, perimetro, diagonal;
+
+            Console.Write("Base do retangulo: ");
+            x = double.Parse(Console.ReadLine(), CI);
+
+            Console.Write("Altura do retangulo: ");
+            y = double.Parse(Console.ReadLine(), CI);
+
+            area = x * y;
+            perimetro = 2 * (x + y);
+            diagonal = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+
+            Console.WriteLine("AREA = " + area.ToString("F4",CI));
+            Console.WriteLine("PERIMETRO = " + perimetro.ToString("F4", CI));
+            Console.WriteLine("DIAGONAL = " + diagonal.ToString("F4", CI));
+            
+        }
+    }
+}
