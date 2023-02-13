b598e5a39a48d5fad03465099dbc0cca29f3a4e5
Intel-CoreDuo <125302044+Intel-CoreDuo@users.noreply.github.com>
2023-02-13 16:49:10 -0300

    Add files via upload

---
+++ csharp/menor_de_tres/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace menor_de_tres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z, menor;
+
+            Console.Write("Primeiro valor: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Segundo valor: ");
+            y = int.Parse(Console.ReadLine());
+            Console.Write("Terceiro valor: ");
+            z = int.Parse(Console.ReadLine());  
+
+            if (x < y && x < z)
+            {
+                menor = x;
+            }
+            else if (y < z)
+            {
+                menor = y;
+            }
+            else
+            {
+                menor = z;
+            }
+            Console.WriteLine("MENOR = " + menor);
+        }
+    }
+}
