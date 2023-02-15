22ccb79ea6d7f7e65e5305122d1291aed96ce2f8
Intel-CoreDuo <125302044+Intel-CoreDuo@users.noreply.github.com>
2023-02-15 09:49:13 -0300

    Add files via upload

---
+++ csharp/terreno/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace terreno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float largura, comprimento, valor_por_m, area, preco;
+            CultureInfo culture = CultureInfo.InvariantCulture;
+
+            Console.Write("Digite a largura do terreno: ");
+            largura = float.Parse(Console.ReadLine(), culture);
+
+            Console.Write("Digite o comprimento do terreno: ");
+            comprimento = float.Parse(Console.ReadLine(), culture);
+
+            Console.Write("Digite o valor do metro quadrado: ");
+            valor_por_m= float.Parse(Console.ReadLine(), culture);
+
+            area = largura * comprimento;
+
+            preco = valor_por_m * area;
+
+            Console.WriteLine("Area do terreno = " + area.ToString("F2", culture));
+            Console.WriteLine("Preco do terreno = " + preco.ToString("F2", culture));
+
+        }
+    }
+}
