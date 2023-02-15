391e842d817730d4da5842f235581cd952cc4cd9
Intel-CoreDuo <125302044+Intel-CoreDuo@users.noreply.github.com>
2023-02-15 09:37:27 -0300

    Add files via upload

---
+++ csharp/duracao/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace duracao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int duracao, horas, minutos, segundos;
+
+            Console.Write("Digite a duração em segundos: ");
+            duracao = int.Parse(Console.ReadLine());
+
+            horas = duracao / 3600;
+            minutos = (duracao % 3600)/60;
+            segundos = duracao % 60;
+
+            Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+        }
+    }
+}
