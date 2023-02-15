9e0418d57fdff96936ae1acb461d28deb46115c3
Intel-CoreDuo <125302044+Intel-CoreDuo@users.noreply.github.com>
2023-02-15 09:38:20 -0300

    Add files via upload

---
+++ csharp/glicose/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace glicose
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float glicose;
+            CultureInfo culture = CultureInfo.InvariantCulture;
+
+            Console.Write("Digite a medida da glicose: ");
+            glicose = float.Parse(Console.ReadLine(), culture);
+
+            if (glicose <= 100)
+            {
+                Console.WriteLine("Classificação: normal");
+            }
+            else if (glicose <= 140)
+            {
+                Console.WriteLine("Classificação: elevado");
+            }
+            else
+            {
+                Console.WriteLine("Classificação: diabetes");
+            }
+        }
+    }
+}
