61f8174db839bb8cd95a8d2bb9a615c9b6756a2c
Intel-CoreDuo <125302044+Intel-CoreDuo@users.noreply.github.com>
2023-02-15 09:36:25 -0300

    Add files via upload

---
+++ csharp/dardo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace dardo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double maior_dist, x, y, z;
+            CultureInfo culture = CultureInfo.InvariantCulture;
+
+            Console.WriteLine("Digite as três distâncias: ");
+            x = double.Parse(Console.ReadLine(), culture);
+            y = double.Parse(Console.ReadLine(), culture);
+            z = double.Parse(Console.ReadLine(), culture);
+
+            if (x > y && x > z)
+            {
+                maior_dist = x;
+            }
+            else if (y > z) 
+            {
+                maior_dist = y;
+            }
+            else
+            {
+                maior_dist = z;
+            }
+
+            Console.WriteLine("MAIOR DISTÂNCIA = " + maior_dist.ToString("F2", culture));
+        
+        
+        }
+    }
+}
