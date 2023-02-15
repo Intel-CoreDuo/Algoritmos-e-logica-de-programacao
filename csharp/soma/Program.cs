42781ec28b410960a4887f185716ab8add0221fb
Intel-CoreDuo <125302044+Intel-CoreDuo@users.noreply.github.com>
2023-02-15 09:45:56 -0300

    Add files via upload

---
+++ csharp/soma/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace soma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int X, Y;
+
+            Console.Write("Digite o valor de X: ");
+            X = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de Y: ");
+            Y = int.Parse(Console.ReadLine());
+
+            int soma = X + Y;
+
+            Console.WriteLine("SOMA = " + soma);
+
+        }
+    }
+}
