939946769a832dbdfb1332d5252796e99c97d7e2
Intel-CoreDuo <125302044+Intel-CoreDuo@users.noreply.github.com>
2023-02-15 09:31:08 -0300

    Add files via upload

---
+++ csharp/circulo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace circulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio;
+            CultureInfo culture = CultureInfo.InvariantCulture; 
+
+            Console.Write("Digite o valor do raio do circulo: ");
+            raio = double.Parse(Console.ReadLine(), culture);
+
+            double area = Math.Pow(raio, 2) * Math.PI;
+
+            Console.WriteLine("AREA = " + area.ToString("F3", culture));
+
+        }
+    }
+}
