4575799f5cc158cab3fa0d5f1fece97ef9fe1d6d
Intel-CoreDuo <125302044+Intel-CoreDuo@users.noreply.github.com>
2023-02-15 09:32:48 -0300

    Add files via upload

---
+++ csharp/consumo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace consumo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int distancia;
+            float gasto, consumo;
+            CultureInfo culture = CultureInfo.InvariantCulture;
+
+            Console.Write("Distância percorrida: ");
+            distancia = int.Parse(Console.ReadLine());
+
+            Console.Write("Combustível gasto: ");
+            gasto = float.Parse(Console.ReadLine(), culture);
+
+            consumo = distancia/ gasto;
+
+            Console.WriteLine("Consumo médio: " + consumo.ToString("F3", culture));
+        }
+    }
+}
