d601bce4a12cd04ae636f3ea1bb982cf9be6fc01
Intel-CoreDuo <125302044+Intel-CoreDuo@users.noreply.github.com>
2023-02-15 09:41:51 -0300

    Add files via upload

---
+++ csharp/multiplos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace multiplos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            Console.WriteLine("Digite dois numeros inteiros: ");
+            x = int.Parse(Console.ReadLine());
+            y= int.Parse(Console.ReadLine());
+
+            if (x % y == 0 || y % x == 0)
+            {
+                Console.WriteLine("São múltiplos");
+            }
+            else
+            {
+                Console.WriteLine("Não são múltiplos");
+            }
+        }
+    }
+}
