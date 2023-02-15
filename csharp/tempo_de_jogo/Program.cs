d6f9c6a910d4bb8f37f9359d348632e4bd2ef83c
Intel-CoreDuo <125302044+Intel-CoreDuo@users.noreply.github.com>
2023-02-15 09:48:05 -0300

    Add files via upload

---
+++ csharp/tempo_de_jogo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace tempo_de_jogo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inicio, fim, duracao;
+
+            Console.Write("Hora inicial: ");
+            inicio = int.Parse(Console.ReadLine());
+
+            Console.Write("Hora final: ");
+            fim = int.Parse(Console.ReadLine());    
+
+            if (inicio < fim)
+            {
+                duracao = fim - inicio;
+            }
+            else
+            {
+                duracao = (24 - inicio) + fim;
+            }
+
+            Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+        }
+    }
+}
