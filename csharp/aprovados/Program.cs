017c7528904e8d711d47e132aef36035e7c7f25a
Intel-CoreDuo <125302044+Intel-CoreDuo@users.noreply.github.com>
2023-02-15 10:03:32 -0300

    Add files via upload

---
+++ csharp/aprovados/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aprovados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
